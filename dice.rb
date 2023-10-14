909995c9f24f360d42a8224cdc5748f354c532a7
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-14 19:28:36 +0000

    dice roll begining

---
+++ dice.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+
+get("/") do
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
